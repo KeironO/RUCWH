5a3c482f78aa98022301e59b6f87b0c25f4bc3d7
KeironO <keiron.oshea@gmail.com>
2019-07-08 20:23:56 +0100

    It works

--- rucwh/app/service.py
+++ rucwh/app/service.py
@@ -1,9 +1,10 @@
 import threading
+from sqlalchemy.orm import sessionmaker
 from rssg import RSClan, RSAccount
 import datetime
 from config import BaseConfig
 from joblib import delayed, Parallel
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, MetaData
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, MetaData, BigInteger
 import os
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -16,23 +17,34 @@ class SkillTable(Base):
     timestamp = Column(DateTime, default=datetime.datetime.utcnow)
     username = Column(String)
     skill_id = Column(Integer)
-    xp = Column(Integer)
+    xp = Column(BigInteger)
     level = Column(Integer)
 
 def _create_engine():
     _base_engine_url = "%s:%s@%s/rucwhdb" % (os.environ["postgresql_usr"], os.environ["postgresql_pass"], BaseConfig.POSTGRESQL_ADDR)
     return create_engine('postgresql+psycopg2://%s' % _base_engine_url, echo=True)
 
-def _get_members_hiscores(name: str):
+def _get_members_hiscores(name: str, engine):
     hiscores = RSAccount(name).hiscores
 
-def _do_service():
-    threading.Timer(BaseConfig.TICKOVER, _do_service).start()
+def _do_service(session):
+    threading.Timer(BaseConfig.TICKOVER, _do_service, [session]).start()
     clan = RSClan(BaseConfig.CLAN_NAME)
 
     for member in clan.clan_members:
         member = RSAccount(member)
         hiscores = member.hiscores
+        if hiscores == None:
+            pass
+        else:
+            for scount, sid  in enumerate(hiscores.keys()):
+                st = SkillTable()
+                st.username = member.username
+                st.skill_id = scount
+                st.xp = int(hiscores[sid]["XP"])
+                st.level = hiscores[sid]["Level"]
+                session.add(st)
+            session.commit()
 
 if __name__ == "__main__":
     engine = _create_engine()
@@ -41,5 +53,6 @@ if __name__ == "__main__":
 
     if not engine.dialect.has_table(engine, SkillTable):
         Base().metadata.create_all(engine)
-
-    _do_service()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    _do_service(session)

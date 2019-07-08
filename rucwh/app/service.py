21d0a6dbc47b171443f7cba3c137ad63ee499dc5
KeironO <keiron.oshea@gmail.com>
2019-07-08 11:48:11 +0100

    Added stuff

--- rucwh/app/service.py
+++ rucwh/app/service.py
@@ -1,8 +1,29 @@
 from rssg import RSClan, RSAccount
+import datetime
 from config import BaseConfig
 from joblib import delayed, Parallel
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+import os
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
+
+class SkillTable(Base):
+    __tablename__ = "skilltable"
+
+    id = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+    username = Column(String)
+    skill_id = Column(Integer)
+    xp = Column(Integer)
+    level = Column(Integer)
+
+def _create_engine():
+    _base_engine_url = "%s:%s@%s/rucwhdb" % (os.environ["postgresql_usr"], os.environ["postgresql_pass"], BaseConfig.POSTGRESQL_ADDR)
+    #print(_engine_url)
 
 if __name__ == "__main__":
+    #engine = create_engine('postgresql+psycopg2://scott:tiger@localhost/mydatabase')
     clan_name = BaseConfig.CLAN_NAME
     clan = RSClan(clan_name)
+    _create_engine()

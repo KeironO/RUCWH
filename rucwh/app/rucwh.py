fd41154979a8bf303ce4ba5ff71e74d2a3c3e13a
KeironO <keiron.oshea@gmail.com>
2019-07-08 21:24:57 +0100

    What the

--- rucwh/app/rucwh.py
+++ rucwh/app/rucwh.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from sqlalchemy import create_engine
+from config import BaseConfig
+import os
 
 app = Flask(__name__)
+
+_base_engine_url = "%s:%s@%s/rucwhdb" % (os.environ["postgresql_usr"], os.environ["postgresql_pass"], BaseConfig.POSTGRESQL_ADDR)
+engine = create_engine('postgresql+psycopg2://%s' % _base_engine_url, echo=True)
 
 @app.route("/")
 def rucwh():
     return render_template("index.html")
 
-
-@app.route("/api/<skill_id>"):
-    return 0
+@app.route("/api/test")
+def test():
+    return jsonify({})

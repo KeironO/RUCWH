3e065b0030afb8443b6c54ea15d8574b4c8127cd
KeironO <keiron.oshea@gmail.com>
2019-07-13 02:14:49 +0100

    This is legit cancer

--- rucwh/app/rucwh.py
+++ rucwh/app/rucwh.py
@@ -1,19 +1,60 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from config import BaseConfig
 import os
-from db import SkillTable
+from db import SkillTable, Account
 
 app = Flask(__name__)
 
 _base_engine_url = "%s:%s@%s/rucwhdb" % (os.environ["postgresql_usr"], os.environ["postgresql_pass"], BaseConfig.POSTGRESQL_ADDR)
-engine = create_engine('postgresql+psycopg2://%s' % _base_engine_url, echo=True)
+engine = create_engine('postgresql+psycopg2://%s' % _base_engine_url, echo=False)
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 @app.route("/")
 def rucwh():
     return render_template("index.html")
 
-@app.route("/api/test/<skill_id>")
-def test(skill_id):
+@app.route("/api",  methods=['POST'])
+def api():
+    req_data = request.get_json()
 
-    return jsonify({"entry" : skill_id})
+    start_time = req_data["start_time"]
+    end_time = req_data["end_time"]
+    skill_id = req_data["skill_id"]
+
+    return_dict = {}
+
+    for account in session.query(Account).all():
+        info = session.query(SkillTable).filter(SkillTable.account_id == account.id).filter(SkillTable.skill_id == skill_id).filter(SkillTable.timestamp >= start_time).filter(SkillTable.timestamp <= end_time).order_by("timestamp").all()
+
+        if len(info) > 1:
+            earliest = info[0]
+            latest = info[-1]
+
+            return_dict[account.username] = {
+                "start_xp" : earliest.xp,
+                "start_lvl" : earliest.level,
+                "end_xp": latest.xp,
+                "end_lvl" : latest.level,
+                "xp_diff" : latest.xp - earliest.xp,
+                "lvl_diff" : latest.level - earliest.level
+            }
+
+        elif len(info) == 1:
+            only_data = info[0]
+
+            return_dict[account.username] = {
+                "start_xp": only_data.xp,
+                "start_lvl": only_data.level,
+                "end_xp": only_data.xp,
+                "end_lvl": only_data.level,
+                "xp_diff": only_data.xp - only_data.xp,
+                "lvl_diff": only_data.level - only_data.level
+            }
+
+
+    return jsonify(return_dict)
--- rucwh/app/wsgi.py
+++ rucwh/app/wsgi.py
@@ -1,4 +1,4 @@
 from rucwh import app
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
